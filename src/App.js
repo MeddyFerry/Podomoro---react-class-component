e56afba46adf034daf63199482f36b917126dd2d
MeddyFerry <Meddyferry@gmail.com>
2022-12-28 10:36:09 +0100

    button + state + ternaire + on click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Timer from "./Timer";
+import app from "./app.css";
 
 const { Component } = require("react");
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <h1>Podomoro Timer</h1>
         <Timer />
       </div>
--- src/Timer.js
+++ src/Timer.js
@@ -1,8 +1,37 @@
 import { Component } from "react";
 
 class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTimerStarted: false, //gère la valeur mon button.
+    };
+  }
+  // va réagir a l'event click de mon button pour changer la valeur de mon Bool 'isTimerStarted'.
+  handleStartTimer = () => {
+    if (this.state.isTimerStarted) {
+      // si vrai
+      this.setState({
+        // alors je le mets à faux
+        isTimerStarted: false,
+      });
+    } else {
+      // et si faux, alors je le mets à vrai
+      this.setState({
+        isTimerStarted: true,
+      });
+    }
+  };
+
   render() {
-    return <p> 00:00:00</p>;
+    return (
+      <>
+        <p> 00:00:00</p>
+        <button onClick={this.handleStartTimer}>
+          {this.state.isTimerStarted ? "stop" : "start"}
+        </button>
+      </>
+    );
   }
 }
 export default Timer;
