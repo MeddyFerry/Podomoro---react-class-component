b63abce0e94848dcb8934b60dbf7e0d8987caadb
MeddyFerry <Meddyferry@gmail.com>
2022-12-30 10:03:51 +0100

    push time into 'newTimers' but pb of same key

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ class App extends Component {
   }
   //timer doit update notre state sinon aucun sens donc :
   saveTime = (time) => {
+    const newTimers = [...this.state.timers]; // copie de timers qui se trouve dans le state
+    newTimers.push(time);
+
     this.setState({
-      timers: [time],
+      timers: newTimers, // ici on crée un tableau avec une nvl valeur
     });
   };
   render() {
